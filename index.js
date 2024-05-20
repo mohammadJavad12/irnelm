81c2a6c9bb190bb009eabd46bd7e11e056f21434
mohammadJavad12 <80960346+mohammadJavad12@users.noreply.github.com>
2024-05-20 22:02:00 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+var i = 0;
+var txt = 'ایرن علم'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("nav-title").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
